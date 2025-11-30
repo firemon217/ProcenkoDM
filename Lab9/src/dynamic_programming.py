bf6e17499a22af7c751830cb47d27620009d1643
Dmitry <pilot7736gives@gmail.com>
2025-11-30 20:46:54 +0300

    Add tasks

--- Lab9/src/dynamic_programming.py
+++ Lab9/src/dynamic_programming.py
@@ -119,3 +119,74 @@ def levenshtein(a, b):
     return dp[n][m]
 # Время: O(n * m)
 # Память: O(n * m)
+
+def lcs_with_reconstruction(a, b):
+    n, m = len(a), len(b)
+
+    # Строим DP-таблицу
+    dp = [[0] * (m + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if a[i - 1] == b[j - 1]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    # Теперь восстанавливаем LCS
+    i, j = n, m
+    lcs_str = []
+
+    while i > 0 and j > 0:
+        # Если символы совпадают - добавляем в ответ
+        if a[i - 1] == b[j - 1]:
+            lcs_str.append(a[i - 1])
+            i -= 1
+            j -= 1
+        else:
+            # Идем туда, где больше значение
+            if dp[i - 1][j] >= dp[i][j - 1]:
+                i -= 1
+            else:
+                j -= 1
+
+    # Разворачиваем, так как восстанавливали с конца
+    lcs_str.reverse()
+
+    return dp[n][m], "".join(lcs_str)
+
+def knapsack_with_items(weights, values, capacity):
+    n = len(weights)
+
+    # Формируем DP-таблицу
+    dp = [[0] * (capacity + 1) for _ in range(n + 1)]
+
+    for i in range(1, n + 1):
+        w = weights[i - 1]
+        v = values[i - 1]
+
+        for cap in range(1, capacity + 1):
+            if w > cap:
+                dp[i][cap] = dp[i - 1][cap]
+            else:
+                dp[i][cap] = max(
+                    dp[i - 1][cap],
+                    dp[i - 1][cap - w] + v
+                )
+
+    # Восстановление предметов
+    result_value = dp[n][capacity]
+    items = []
+
+    cap = capacity
+    i = n
+
+    while i > 0:
+        # Если значение отличается от верхней строки — предмет был взят
+        if dp[i][cap] != dp[i - 1][cap]:
+            items.append(i - 1)  # сохраняем индекс предмета
+            cap -= weights[i - 1]
+        i -= 1
+
+    items.reverse()
+    return result_value, items
